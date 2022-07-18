c50724a8edc0dbe724f67808e8d9afbb85b92aa0
MuminovSardor <muminovsardor07@gmail.com>
2022-07-18 12:58:58 +0500

    2,3,5,6 - finish

--- 1 Основы JavaScript/3 Alert Confirm Prompt.js
+++ 1 Основы JavaScript/3 Alert Confirm Prompt.js
@@ -1,1 +1,20 @@
 "use strict";
+
+// alert('Hello');
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +promt("Вам есть 18?", "");
+// console.log(typeof(answer));
+
+// const answer = +promt("Вам есть 18?", "");
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = promt('Как ваше имя?', '');
+// answers[1] = promt('Как ваше фамилия?', '');
+// answers[2] = promt('Сколько вам лет?', '');
+
+// document.write(answers);
--- 1 Основы JavaScript/5 Интерполяция (ES6).js
+++ 1 Основы JavaScript/5 Интерполяция (ES6).js
@@ -1,1 +1,15 @@
 "use strict";
+
+// const category = 'toys';
+
+// console.log('https://someurl.com/' + category + '/' + '5');
+
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+
+const user = "Sardor";
+
+alert(`Привет, ${user}`);
