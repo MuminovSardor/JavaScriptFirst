fba17f7cbf47aa63b1897481fc698a029779efda
MuminovSardor <muminovsardor07@gmail.com>
2022-07-19 15:39:09 +0500

    finish

--- 1 Основы JavaScript/6 Операторы в JS.js
+++ 1 Основы JavaScript/6 Операторы в JS.js
@@ -3,14 +3,17 @@
 // console.log('arr' + " - object");
 // console.log(4 + +"5");
 
-let incr = 10,
-    decr = 10;
+// let incr = 10,
+//     decr = 10;
 
 // incr++;
 // decr--;
 
-console.log(++incr);
-console.log(--decr);
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(incr++);
+// console.log(decr--);
 
 // console.log(5%2);
 
