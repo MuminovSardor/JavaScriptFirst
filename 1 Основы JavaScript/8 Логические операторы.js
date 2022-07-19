edbfe224cdcfb5028e8c56b95ee6523bb376a19c
MuminovSardor <muminovsardor07@gmail.com>
2022-07-19 21:53:28 +0500

    7 - finish, 8 - половина

--- 1 Основы JavaScript/8 Логические операторы.js
+++ 1 Основы JavaScript/8 Логические операторы.js
@@ -1,1 +1,33 @@
 "use strict";
+
+// const hamburger = true;
+// const fries = true;
+
+// if(hamburger && fries) {
+//     console.log('Я сыт!');
+// }
+
+// console.log((hamburger && fries));
+
+// const gamburger = true;
+// const ffries = false;
+
+// if(gamburger && ffries) {
+//     console.log('Я сыт!');
+// }
+
+// console.log((gamburger && ffries));
+
+
+
+const hamburger = 2;
+const fries = 1;
+
+if(hamburger ===3 && fries) {
+    console.log('Все сыты!');
+} else {
+
+}
+
+
+
