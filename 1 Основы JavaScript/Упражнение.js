da526d13d42a2846a49d8cab76c336f17ead4d24
MuminovSardor <muminovsardor07@gmail.com>
2022-07-27 15:04:34 +0500

    finish

--- 1 Основы JavaScript/Упражнение.js
+++ 1 Основы JavaScript/Упражнение.js
@@ -1,8 +1,72 @@
 "use strict";
 
 //? 1
-let userName = 'John';
-let number = 25;
-number = 24;
+// let userName = 'John';
+// let number = 25;
+// number = 24;
 
-console.log(number);
+// console.log(number);
+
+
+
+//? 2
+// const storeDescription = {
+//     budget: 10000,
+//     employees: ['Sardor', 'Asadbek', 'Otabek'],
+//     products: {
+//         tShirt: 20,
+//         shirt: 25
+//     },
+//     open: true
+// };
+
+
+
+//? 3
+function firstTask() {
+    for(let i = 5; i < 11; i++) {
+        console.log(i);
+    }
+}
+
+firstTask();
+
+function secondTask() {
+    for (let i = 20; i > 9; i--) {
+        console.log(i);
+        if (i == 13) {
+            break;
+        }
+    }
+}
+
+secondTask();
+
+function thirdTask() {
+
+
+}
+
+
+
+// for (let i = 2; i <= 16; i++) {
+//     if (i % 2 === 0) {
+//         continue;
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+function fourthTask() {
+
+
+}
+
+
+function fifthTask() {
+    const arrayOfNumbers = [];
+
+
+
+    return arrayOfNumbers;
+}
