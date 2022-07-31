e5d0fe696db86a58782f0e41489705b642ed1988
MuminovSardor <muminovsardor07@gmail.com>
2022-08-01 01:05:26 +0500

    Упр 4 - half

--- 1 Основы JavaScript/Упражнение.js
+++ 1 Основы JavaScript/Упражнение.js
@@ -107,3 +107,71 @@ for (let i = 10; i <= 40; i++) {
     }
 }
 console.log(arr);
+
+
+
+//? 4
+
+function firstTask() {
+    const arr = [3, 5, 8, 16, 20, 23, 50];
+    const result = [];
+
+    for( let i = 0; i < arr.length; i++) {
+        result[i] = arr[i];
+    }
+
+    console.log(result);
+
+
+
+    return result;
+}
+
+firstTask();
+
+const qwe = ["Sardor", 'Asadbek'];
+const asd = [];
+
+asd[0] = qwe[1];
+
+qwe[0] = 'Otabek';
+
+console.log(asd);
+
+let zxc = 'Sardor';
+let sara = 'Asadbek';
+console.log(zxc + sara);
+console.log(`${zxc} ${sara}`);
+
+
+function secondTask() {
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+    for (let i = 0; i < data.length; i++) {
+        if (typeof(data[i]) === 'number') {
+            data[i] = data[i] * 2;
+        } else {
+            data[i] = `${data[i]} - done`;
+        }
+    }
+
+    console.log(data);
+
+    return data;
+}
+
+secondTask();
+
+
+
+
+function thirdTask() {
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+    const result = [];
+
+
+
+    return result;
+}
+
+thirdTask();
