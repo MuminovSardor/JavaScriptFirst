09aa8b0fada544565b2bbad53fe33a9bb3a2adea
MuminovSardor <muminovsardor07@gmail.com>
2022-08-12 19:48:10 +0500

    Упр 4 - secondTask, thirdTask

--- 1 Основы JavaScript/Упражнение.js
+++ 1 Основы JavaScript/Упражнение.js
@@ -94,84 +94,86 @@
 
 // fifthTask();
 
-
-
-
-
-//! ===================================
-let arr = [];
-
-for (let i = 10; i <= 40; i++) {
-    if (i % 2 == 0) {
-        arr[i - 10] = i;
-    }
-}
-console.log(arr);
-
-
-
 //? 4
 
-function firstTask() {
-    const arr = [3, 5, 8, 16, 20, 23, 50];
-    const result = [];
+// function firstTask() {
+//     const arr = [3, 5, 8, 16, 20, 23, 50];
+//     const result = [];
 
-    for( let i = 0; i < arr.length; i++) {
-        result[i] = arr[i];
-    }
+//     for( let i = 0; i < arr.length; i++) {
+//         result[i] = arr[i];
+//     }
 
-    console.log(result);
-
-
-
-    return result;
-}
-
-firstTask();
-
-const qwe = ["Sardor", 'Asadbek'];
-const asd = [];
-
-asd[0] = qwe[1];
-
-qwe[0] = 'Otabek';
-
-console.log(asd);
-
-let zxc = 'Sardor';
-let sara = 'Asadbek';
-console.log(zxc + sara);
-console.log(`${zxc} ${sara}`);
+//     console.log(result);
 
 
-function secondTask() {
-    const data = [5, 10, 'Shopping', 20, 'Homework'];
 
-    for (let i = 0; i < data.length; i++) {
-        if (typeof(data[i]) === 'number') {
-            data[i] = data[i] * 2;
-        } else {
-            data[i] = `${data[i]} - done`;
-        }
-    }
+//     return result;
+// }
 
-    console.log(data);
+// firstTask();
 
-    return data;
-}
+// const qwe = ["Sardor", 'Asadbek'];
+// const asd = [];
 
-secondTask();
+// asd[0] = qwe[1];
+
+// qwe[0] = 'Otabek';
+
+// console.log(asd);
+
+// let zxc = 'Sardor';
+// let sara = 'Asadbek';
+// console.log(zxc + sara);
+// console.log(`${zxc} ${sara}`);
 
 
 
 
-function thirdTask() {
-    const data = [5, 10, 'Shopping', 20, 'Homework'];
-    const result = [];
+
+
+// function secondTask() {
+//     const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+//         for(let i = 0; i < data.length; i++) {
+//            if (typeof (data[i]) === 'number'){
+//                 data[i] = data[i] * 2;
+//            } else {
+//             data[i] = `${data[i]} - done`;
+//            }
+//         }
+
+
+//     console.log(data);
+
+//     return data;
+// }
+
+// secondTask();
 
 
 
-    return result;
-}
 
-thirdTask();
+// function thirdTask() {
+//     const data = [5, 10, 'Shopping', 20, 'Homework'];
+//     const result = [];
+
+//         for(let i = 1; i <= data.length; i++) {
+//             result[i - 1] = data[data.length - i];
+//         }
+
+//         console.log(result);
+
+//     return result;
+// }
+
+// thirdTask();
+
+
+
+
+
+//? 5
+
+
+
