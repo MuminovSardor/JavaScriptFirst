8115854adfad5cc18208568df200be0e920c14e0
MuminovSardor <muminovsardor07@gmail.com>
2022-08-16 23:48:05 +0500

    Упр 5

--- 1 Основы JavaScript/Упражнение.js
+++ 1 Основы JavaScript/Упражнение.js
@@ -175,22 +175,29 @@
 
 //? 5
 
+/*
 
+     *
+    ***
+   *****
+  *******
+ *********
+***********
 
-const lines = 5;
-let result = '';
-// Проверяется именно переменная result, формируйте строку в ней
+*/
 
+// let result = '';
 
-for(let i = 0; i <= lines; i++) {
-    for(let j = 0; j < lines - i; j++) {
-        result += " ";
-    }
-    for(let j = 0; j < 2 * i + 1; j++) {
-        result += "*";
-    }
-    result += "\n";
-}
+// for(let i = 0; i <= 5; i++) {
+//     for(let j = 0; j < 5 - i; j++) {
+//         result = result + ' ';
+//     }
 
+//     for(let j = 0; j < i * 2 + 1; j++) {
+//         result = result + '*';
+//     }
 
-console.log(result);
+//     result = result + '\n';
+// }
+
+// console.log(result);
